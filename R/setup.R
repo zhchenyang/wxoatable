738da722458c71046f7a42776943aba70e168ec6
zhchenyang <heiyangs@163.com>
2020-10-16 17:29:42 +0800

    update

--- R/setup.R
+++ R/setup.R
@@ -6,10 +6,8 @@ appsecret = rstudioapi::askForSecret("appsecret")
 APPID(appid)
 APPSecret(appsecret)
 
-df = rio::import("data/getusercumulate.csv")
-start_date = as.Date(tail(df$ref_date, 1))+1
+# update user cumulate
 
 get_token()
 
-data = get_data(keyword = "getusercumulate", start = start_date, append = "data/getusercumulate.csv")
-# append works
+update_data(append = "data/getusercumulate.csv")
